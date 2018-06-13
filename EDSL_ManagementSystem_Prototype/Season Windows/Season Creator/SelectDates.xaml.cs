6c0d380a5dd100642e3650ecdf10ee99171b0b19
kybanez12 <kybanez12@gmail.com>
2018-06-13 11:43:32 +1000

    Add project files.

---
+++ EDSL_ManagementSystem_Prototype/Season Windows/Season Creator/SelectDates.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EDSL_ManagementSystem_Prototype.Season_Windows.Season_Creator
+{
+    /// <summary>
+    /// Interaction logic for SelectDates.xaml
+    /// </summary>
+    public partial class SelectDates : Window
+    {
+        public SelectDates()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ EDSL_ManagementSystem_Prototype/Season Windows/Season.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EDSL_ManagementSystem_Prototype
+{
+    /// <summary>
+    /// Interaction logic for Season.xaml
+    /// </summary>
+    public partial class Season : Window
+    {
+        public Season()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
